d4c1a70c3fa6ad48d8d05c6be7f085b7f942b7ad
Abdelrahman Hassan <boba9963@gmail.com>
2024-01-30 02:24:54 +0200

    some edits

--- routes/category/api.php
+++ routes/category/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Category\CategoryController;
 
-Route::prefix('admin')->middleware('auth:adminapi')->group(function(){
+Route::prefix('admin')->middleware('auth:adminapi,userapi')->group(function(){
     Route::apiResource('category',CategoryController::class);
     Route::post('category/search',[CategoryController::class,'search']);
 });
--- routes/product/api.php
+++ routes/product/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Product\ProductController;
 
-Route::prefix('admin')->middleware(['auth:adminapi'])->group(function(){
+Route::prefix('admin')->middleware('auth:adminapi,userapi')->group(function(){
     Route::apiResource('product',ProductController::class);
     Route::post('product/search',[ProductController::class,'search']);
 });
